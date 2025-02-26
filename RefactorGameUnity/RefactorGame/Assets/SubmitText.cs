250bc54ae3864136d7b92f658d6b2457b6bec062
Christopher Engelbart <cengelbart39@protonmail.com>
2025-02-26 14:04:06 -0500

    dquote> - Add InGameTextEditor dquote> - Temp fix for chef movement

--- RefactorGameUnity/RefactorGame/Assets/SubmitText.cs
+++ RefactorGameUnity/RefactorGame/Assets/SubmitText.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using InGameTextEditor;
 using ParserLibrary;
 using RefactorLang;
 using RefactorLib;
+using UnityEngine.UI;
+using System;
 
 public class SubmitText : MonoBehaviour
 {
-    public TMP_InputField inputField;
+    public InGameTextEditor.TextEditor textEditor;
     public GameObject chef;
 
     public void OnSubmit()
     {
-        List<UnityPackage> outputLog = ScriptCompiler.Compile(inputField.text);
+        string text = textEditor.Text;
+
+        List<UnityPackage> outputLog = ScriptCompiler.Compile(text);
 
         chef.GetComponent<ChefExecute>().Execute(outputLog);
     }
