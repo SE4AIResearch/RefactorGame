c40cfac9e2cbef0534b50e8870c3c58fe9c8a56d
Christopher Engelbart <cengelbart39@protonmail.com>
2025-03-31 15:08:23 -0400

    Add level detail

--- RefactorGameUnity/RefactorGame/Assets/LevelSelectHandler.cs
+++ RefactorGameUnity/RefactorGame/Assets/LevelSelectHandler.cs
@@ -1,15 +1,16 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RefactorLang;
-using System.Linq;
 using System;
-using UnityEngine.SceneManagement;
+using TMPro;
+using System.Text.Json;
+using UnityEngine.UI;
 
 public class LevelSelectHandler : MonoBehaviour
 {
     public Camera Camera;
     public GameObject LevelModuleLocations;
+    public GameObject LevelDetail;
 
     private Vector3 CameraTarget;
     private float CameraSmoothSpeed = (1f / 32f);
@@ -48,6 +49,18 @@ public class LevelSelectHandler : MonoBehaviour
         CameraTarget = new Vector3(target.transform.position.x, Camera.transform.position.y, Camera.transform.position.z);
 
         transform.Find("Selected").position = target.transform.position;
+        
+        float detailY = target.transform.position.y - UpdateFactor(levelIndex);
+        Vector3 detailTarget = new Vector3(Camera.transform.position.x, detailY, target.transform.position.z);
+        LevelDetail.transform.position = detailTarget;
+
+        string puzzleName = puzzleNames[SelectedLevel - 1];
+        TextAsset json = Resources.Load<TextAsset>(@$"Puzzles/Json/{puzzleName}");
+        Puzzle puzzle = JsonSerializer.Deserialize<Puzzle>(json.text);
+
+        LevelDetail.transform.Find("Name").GetComponent<TextMeshProUGUI>().text = puzzle.Name;
+        LevelDetail.transform.Find("Story").GetComponent<TextMeshProUGUI>().text = puzzle.StoryPrompt;
+        LayoutRebuilder.ForceRebuildLayoutImmediate(LevelDetail.GetComponent<RectTransform>());
     }
 
     // Start is called before the first frame update
@@ -71,6 +84,24 @@ public class LevelSelectHandler : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Return))
         {
             LevelLoader.LoadPuzzleScene(puzzleNames[SelectedLevel - 1]);
+        }
+    }
+
+    private float UpdateFactor(int index)
+    {
+        switch (index)
+        {
+            case int n when (n >= 0 && n <= 4):
+                return 2f;
+
+            case int n when (n >= 5 && n <= 9):
+                return -2.5f;
+
+            case int n when (n >= 10 && n <= 11):
+                return 2f;
+
+            default:
+                throw new ArgumentException("Invalid level index");
         }
     }
 }
