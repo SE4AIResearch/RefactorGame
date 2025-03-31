9e8368ceff59b18ef5b14a0c5c801cf3e1845e01
Christopher Engelbart <cengelbart39@protonmail.com>
2025-03-31 12:00:36 -0400

    Rename 'UpdatedScene' to 'Level'

--- RefactorGameUnity/RefactorGame/Assets/ChefExecute.cs
+++ RefactorGameUnity/RefactorGame/Assets/ChefExecute.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using TMPro;
-using UnityEditor.UI;
 using UnityEngine;
 using static UnityEngine.Rendering.ReloadAttribute;
 
